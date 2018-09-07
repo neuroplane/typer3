ff34b08ad039ccaef2fbb3ecc4cbac3ea01f564c
neuroplane <aleksey@boyarkin.com>
2018-09-07 08:59:29 +0300

    Commit

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 folder="/media/$USER/"
 options=($(ls $folder))
 folderlist=$(ls $folder | wc -l)
 x=0
 while [ $x -lt $folderlist ]
-        do
-                echo -e "$x. ${options[$x]}"
-                x=$(( $x + 1 ))
-        done
+  do
+    echo -e "$x. ${options[$x]}"
+    x=$(( $x + 1 ))
+  done
 echo "Choose USB drive containing 'Video' folder"
 read selector
 echo ${options[$selector]} > ./config/videofolder.conf
-[ ! -d "$folder${options[$selector]}/Videos" ] && echo "NO VIDEO FOLDER $folder${options[$selector]}/Videos" || echo "VIDEO FOLDER EXISTS";echo ${options[$selector]} > ./config/videofolder.conf; cat ./confi$
+#
+[ ! -d "$folder${options[$selector]}/Videos" ] && echo "NO FOLDER $folder${options[$selector]}/Videos" || echo "FOLDER OK";echo ${options[$selector]} > ./config/videofolder.conf
